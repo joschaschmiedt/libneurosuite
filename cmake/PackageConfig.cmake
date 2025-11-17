80c917d5eb0cd0158da4055b65c48a0e7d0ea081
Joscha Schmiedt <schmiedt@uni-bremen.de>
2025-11-17 16:04:14 +0100

    Update package config

--- cmake/PackageConfig.cmake
+++ cmake/PackageConfig.cmake
@@ -1,14 +1,8 @@
 # Find packages of dependencies
 include(CMakeFindDependencyMacro)
 
-set(@PROJECT_NAME@_WITH_QT4 @WITH_QT4@)
-if(@PROJECT_NAME@_WITH_QT4)
-    find_dependency(Qt4 4.8.0)
-else()
-    find_dependency(Qt5Widgets)
-    find_dependency(Qt5PrintSupport)
-    find_dependency(Qt5WebKitWidgets)
-endif()
+# Find Qt6 dependencies
+find_dependency(Qt6 REQUIRED COMPONENTS Widgets PrintSupport WebEngineWidgets)
 
 list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}/modules")
 
