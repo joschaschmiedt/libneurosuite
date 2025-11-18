4394f09680a1e809313c08a14a9a41f4ebb5cf87
Joscha Schmiedt <schmiedt@uni-bremen.de>
2025-11-18 11:21:45 +0100

    Add XML to PackageConfig

--- cmake/PackageConfig.cmake
+++ cmake/PackageConfig.cmake
@@ -2,7 +2,7 @@
 include(CMakeFindDependencyMacro)
 
 # Find Qt6 dependencies
-find_dependency(Qt6 REQUIRED COMPONENTS Widgets PrintSupport WebEngineWidgets)
+find_dependency(Qt6 REQUIRED COMPONENTS Widgets PrintSupport WebEngineWidgets Xml)
 
 list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}/modules")
 
